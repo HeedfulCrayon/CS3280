7c45377d00f19353d78914e76f8290622c2026cf
Nathan Borup <nathan.borup@gmail.com>
2016-01-26 18:40:14 -0700

    Assignment 2 - Roll working

---
+++ Assignment2/Assignment2/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startRoll = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startRoll
+            // 
+            this.startRoll.Location = new System.Drawing.Point(322, 141);
+            this.startRoll.Name = "startRoll";
+            this.startRoll.Size = new System.Drawing.Size(101, 27);
+            this.startRoll.TabIndex = 0;
+            this.startRoll.Text = "Roll";
+            this.startRoll.UseVisualStyleBackColor = true;
+            this.startRoll.Click += new System.EventHandler(this.startRoll_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.ImageLocation = "die1.gif";
+            this.pbImage.Location = new System.Drawing.Point(322, 23);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(101, 89);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 1;
+            this.pbImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 440);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.startRoll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startRoll;
+        private System.Windows.Forms.PictureBox pbImage;
+    }
+}
+
---
+++ Assignment2/Assignment2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startRoll_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                int seed = rand.Next(1, 6);
+                pbImage.Image = Image.FromFile("die" + seed.ToString() + ".gif");
+                pbImage.Refresh();
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
